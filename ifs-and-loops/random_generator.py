214231bb40b88e562fd78646ea20d584979bdc1d
raketti <raketti@mailbox.org>
2023-05-17 13:16:07 +0300

    Comments and refactoring

--- ifs-and-loops/random_generator.py
+++ ifs-and-loops/random_generator.py
@@ -3,14 +3,13 @@ h = 24              # Hours
 d = 7               # Days
 sampleSize = h * d  # Calculate the sample size
   
-# Test wit a random number generator to get a 
+# Test wit a random number generator to get some data
 def random_generator():
   import random
 
   for count in range(1):
-    
-    i = random.randint(1, 25)
-    
+    # So we have winters in Finland, let's generate some colder temperatures also
+    i = random.randint(-30, 30)
     f = open(outputFile, 'a')
     f.write((str(i) + "\n"))
     f.close()
@@ -18,26 +17,34 @@ def random_generator():
 random_generator()
 
 # Keep data until we have enough samples,
-# if sampleSize is met, start removing old data
+# If sampleSize is met, start removing old data
 def removeFirstLine():
 
 # Count the number of lines
   with open(outputFile, 'r') as lc:
     line_count = sum(1 for line in lc)
-    print('Total Number of lines:', line_count)
 
 # If we have enough samples, we remove the first entry
   if line_count >= sampleSize:
     with open(outputFile, 'r+') as fr:
+      
         # read an store all lines into list
         lines = fr.readlines()
+        
         # move file pointer to the beginning of a file
         fr.seek(0)
+        
         # truncate the file
         fr.truncate()
 
         # start writing lines except the first line
         fr.writelines(lines[1:])
   else:
-    print("Not enough samples, will keep data")
+    print("Not enough samples, sample size:", line_count, "Required:", sampleSize)
+
 removeFirstLine()
+
+def calculateAverage():
+  pass
+  # Sum the line values and calculate average/median
+calculateAverage()
